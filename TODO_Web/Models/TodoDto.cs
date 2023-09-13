55e5fc17af208361d7e250359e88aea792c3aba6
Reagan27 <luttareagan22@gmail.com>
2023-09-13 15:02:17 +0300

    commit changes

---
+++ TODO_Web/Models/TodoDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TODO_Web.Models
+{
+    public class TodoDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Priority { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; } = false;
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
--- TODO_Web/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TODO_Web.Models
-{
-    public class TodoItem
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public bool IsDone { get; set; }
-    }
-}
